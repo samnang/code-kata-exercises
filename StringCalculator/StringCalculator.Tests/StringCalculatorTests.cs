1f7e13a358dc08206a3de5ff3f011c1e8f28243c
Samnang <samnang.chhun@gmail.com>
2009-12-05 10:47:14 +0700

    Sum be able to calculate with multiple numbers

--- StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
+++ StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
@@ -1,9 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Diagnostics;
+using NUnit.Framework;
 
 namespace StringCalculator.Tests {
+    [TestFixture]
     public class StringCalculatorTests {
+        private StringCalculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new StringCalculator();
+        }
+
+        [Test]
+        public void Sum_EmptyString_Zero()
+        {
+            var expectedResult = 0;
+
+            var result = _calculator.Sum(string.Empty);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Sum_Null_Zero()
+        {
+            var expectedResult = 0;
+
+            int result = _calculator.Sum(null);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(1, "1")]
+        [TestCase(3, "3")]
+        public void Sum_OneNumber_Number(int expectedResult, string numbers)
+        {
+            var result = _calculator.Sum(numbers);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(5, "2,3")]
+        [TestCase(10, "5,3,2")]
+        public void Sum_MulipleNumbers_TotalOfNumbers(int expectedResult, string numbers)
+        {
+            var result = _calculator.Sum(numbers);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Sum_MultipleNumbersWithSpace_TotalOfNumbers()
+        {
+            var expectedResult = 5;
+
+            int result = _calculator.Sum("2, 3");
+
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
---
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace StringCalculator.Tests
+{
+    public class StringCalculator
+    {
+        public int Sum(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            if (IsMultipleNumbers(numbers))
+            {
+                return numbers.Split(',').Sum(number => int.Parse(number));
+            }
+
+            return int.Parse(numbers);
+        }
+
+        private bool IsMultipleNumbers(string numbers)
+        {
+            return numbers.Contains(",");
+        }
+    }
+}
