3a5657bb5385114e4482c643a237945da7b11fa2
Samnang <samnang.chhun@gmail.com>
2010-01-28 20:28:01 +0700

    Added String Set in Javascript project

---
+++ StringSet_JS/string-set-spec.js
@@ -0,0 +1,11 @@
+Screw.Unit(function() {
+  describe("Add item(s)", function() {
+  	it("should increment items' count", function(){
+  		
+  	});
+  	
+  	it("should return the added value", function(){
+  		
+  	});        	
+	});
+});
