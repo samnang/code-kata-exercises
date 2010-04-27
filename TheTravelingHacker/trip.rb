b79d571ab87717a5d0d71fbbee83564cd694580b
Samnang <samnang.chhun@gmail.com>
2010-04-27 23:32:13 +0700

    Finished specifications for The Traveling Hacker

--- TheTravelingHacker/trip.rb
+++ TheTravelingHacker/trip.rb
@@ -11,10 +11,48 @@ class Trip
 		@stops << stop_name
 	end
 	
+	def total_distance
+		calculate_total_distance_by_point_to_point
+	end
+	
+	def to_s
+		report = ""
+		
+		total_miles = calculate_total_distance_by_point_to_point do |origin, destination, miles|
+										report += "#{origin} -> #{destination}: #{miles}\n"
+									end
+										
+		report += "\nTotal: #{total_miles}"
+	end
+	
 	private
 	
 	def valid_location?(location)
 		@routing_service.get("/validate", :location => location)["valid"]
+	end
+	
+	def calculate_total_distance_by_point_to_point
+		zero_mile = 0
+		return zero_mile if @stops.length < 2
+		
+	  total_miles = 0
+		@stops.each_with_index do |stop, index|
+			origin = stop
+			destination = @stops[index + 1]
+			
+			miles = calculate_between_distances(origin,  destination)
+			total_miles += miles
+			
+			yield(origin, destination, miles) if block_given?
+			
+			break if index == @stops.length - 2
+		end
+		
+		total_miles
+	end
+	
+	def calculate_between_distances(origin, destination)
+		@routing_service.get("/distance", :origin => origin, :destination => destination)["miles"]
 	end
 	
 	InvalidStopError = Class.new(StandardError)
--- TheTravelingHacker/trip_test.rb
+++ TheTravelingHacker/trip_test.rb
@@ -12,7 +12,7 @@ class TripTest < Test::Unit::TestCase
 	
 	context "adding a stop" do
 		test "stop count is initially 0" do
-			assert_equal 0, @trip.stops.length
+			assert_equal(0, @trip.stops.length)
 		end
 		
 		test "adding a valid stop increases stops.length by 1" do
@@ -37,6 +37,57 @@ class TripTest < Test::Unit::TestCase
 		end
 	end
 	
+	context "computing distance" do
+		test "should return 0 when stops < 2" do
+			valid_stop("Hartford, CT")
+			
+			@trip.add_stop("Hartford, CT")
+			
+			assert_equal(0, @trip.total_distance)
+		end
+		
+		test "should compute point to point disstance with two stops" do
+			all_stops_valid
+			distance_between "New Haven, CT", "Naugatuck, CT", 17.4
+			
+			@trip.add_stop("New Haven, CT")
+			@trip.add_stop("Naugatuck, CT")
+			
+			assert_equal(17.4, @trip.total_distance)
+		end
+		
+		test "should compute leg by leg distance for N stops" do
+			all_stops_valid
+			
+			distance_between "Point A", "Point B", 6
+			distance_between "Point B", "Point C", 9
+			distance_between "Point C", "Point D", 4
+			distance_between "Point D", "Point E", 13
+			
+			%w[A B C D E].each do |point|
+				@trip.add_stop("Point #{point}")
+			end
+			
+			assert_equal(32, @trip.total_distance)
+		end
+	end
+	
+	context "trip mileage report" do
+		test "should have a meaningful string representation" do
+			all_stops_valid
+			
+			distance_between "Point A", "Point B", 6
+			distance_between "Point B", "Point C", 9
+			
+			%w[A B C].each do |point|
+				@trip.add_stop("Point #{point}")
+			end
+			
+			assert_equal("Point A -> Point B: 6\nPoint B -> Point C: 9\n\n"+ 
+										"Total: 15", @trip.to_s)
+		end
+	end
+	
 	def valid_stop(location)
 		@routing_service.expects(:get).
 										 with("/validate", :location => location).
@@ -47,5 +98,17 @@ class TripTest < Test::Unit::TestCase
 		@routing_service.expects(:get).
 										 with("/validate", :location => location).
 										 returns("valid" => false)
+	end
+	
+	def all_stops_valid
+		@routing_service.stubs(:get).with() { |route, params| route == "/validate" }.
+																 returns("valid" => true) 
+	end
+	
+	def distance_between(origin, destination, miles)
+		@routing_service.expects(:get).
+										 with("/distance", :origin => origin,
+										 									 :destination => destination).
+										 returns("miles" => miles)
 	end
 end
