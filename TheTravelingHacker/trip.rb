b261636691ff19b7cc7c1f21b0f7d8e63ca174a9
Samnang <samnang.chhun@gmail.com>
2010-04-27 21:46:04 +0700

    Added stops datastore

--- TheTravelingHacker/trip.rb
+++ TheTravelingHacker/trip.rb
@@ -1,11 +1,14 @@
 class Trip
+	attr_reader :stops
 	
 	def initialize(routing_service)
 		@routing_service = routing_service
+		@stops = []
 	end
 	
 	def add_stop(stop_name)
 		raise InvalidStopError unless valid_location?(stop_name)
+		@stops << stop_name
 	end
 	
 	private
--- TheTravelingHacker/trip_test.rb
+++ TheTravelingHacker/trip_test.rb
@@ -5,28 +5,47 @@ require	'contest'
 require 'mocha'
 
 class TripTest < Test::Unit::TestCase
+	setup do
+		@routing_service = mock("RoutingService")
+		@trip = Trip.new(@routing_service)
+	end
+	
 	context "adding a stop" do
-		test "can add a valid stop without errors" do
-			routing_service = mock('RoutingService')
-			routing_service.expects(:get).
-											with("/validate", :location => "Hartford, CT").
-											returns("valid" => true)
+		test "stop count is initially 0" do
+			assert_equal 0, @trip.stops.length
+		end
+		
+		test "adding a valid stop increases stops.length by 1" do
+			valid_stop("Hartford, CT")
+			valid_stop("Quahog, RI")
 			
-			trip = Trip.new(routing_service)
-			trip.add_stop("Hartford, CT")
+			@trip.add_stop("Hartford, CT")
+			assert_equal(1, @trip.stops.length)
+			
+			@trip.add_stop("Quahog, RI")
+			assert_equal(2, @trip.stops.length)
 		end
 		
 		test "raises an error when an invalid stop is added" do
-			routing_service = mock('RoutingService')
-			routing_service.expects(:get).
-											with("/validate", :location => "Fakevilla, AR").
-											returns("valid" => false)
-											
-			trip = Trip.new(routing_service)
+			invalid_stop("Fakevilla, AR")
 			
 			assert_raises(Trip::InvalidStopError) do
-				trip.add_stop("Fakevilla, AR")
+				@trip.add_stop("Fakevilla, AR")
 			end
+			
+			assert_equal(0, @trip.stops.length)
 		end
+	end
+	
+	def valid_stop(location)
+		@routing_service.expects(:get).
+										 with("/validate", :location => location).
+										 returns("valid" => true)
+	end
+	
+	def invalid_stop(location)
+		@routing_service.expects(:get).
+										 with("/validate", :location => location).
+										 returns("valid" => false)
 	end
 end
